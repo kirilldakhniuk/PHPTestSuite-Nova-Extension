f2497244d544de6ca24a71a194810927ae56cf27
Kyrylo Dakhniuk <kirilldakhnyuk@gmail.com>
2024-02-22 12:38:43 +0200

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+👋 Hello! The extension still in the early development phase. Bugs and odd behaviours are expected 😅.
+
+
+**PHPTestSuite** provides sidebar integration with **PHPUnit** and **PEST** testing tools.
+
+## Requirements
+
+💡 In order to use this extension project must contain composer.json file with a PHPUnit or PEST installed.
+
+## Usage
+
+* Use PHPTestSuite sidebar (double click on the test inside a file will run it or use context menu to run a file)
+- Open the command palette and type `PhpTestSuite`, `RunFile`, etc.
+
+### Configuration
+🎈 There's no configuration at this point.
+
+### Community
+👋 That's it! Hope you will enjoy using it!
+
+P.S. Feel free to create issues, discussion, etc. 😉
+
+### Roadmap
+- [ ] Create a way to show that test is running
+- [ ] Show success or error icon right next to each test in the sidebar
+- [ ] Run a single test in file from the command palette 
+- [ ] Support different projects architecture (DDD, Modules, etc…)
+- [ ] Better tests filtering, right now there’s known issues with a regex
+- [ ] Better folding in the sidebar
---
+++ Scripts/main.js
@@ -0,0 +1,199 @@
+var treeView = null;
+var testRunner = null;
+
+
+exports.activate = function() {
+    // Do work when the extension is activated
+    const compositeDisposable = new CompositeDisposable();
+    
+    // Create the TreeView
+    treeView = new TreeView("phptestsuite", {
+        dataProvider: new TestDataProvider()
+    });
+    
+    treeView.onDidChangeSelection((selection) => {
+        // console.log("New selection: " + selection.map((e) => e.name));
+    });
+    
+    treeView.onDidExpandElement((element) => {
+        // console.log("Expanded: " + element.name);
+    });
+    
+    treeView.onDidCollapseElement((element) => {
+        // console.log("Collapsed: " + element.name);
+    });
+    
+    treeView.onDidChangeVisibility(() => {
+        // console.log("Visibility Changed");
+    });
+    
+    // TreeView implements the Disposable interface
+    nova.subscriptions.add(treeView);
+    
+    testRunner = getTestRunner();
+}
+
+exports.deactivate = function() {
+    // Clean up state before the extension is deactivated
+}
+
+nova.commands.register("phptestsuite.runFile", (editor) => {
+  const parts = editor.activeTextEditor.document.path.split('/');
+  const testFilename = parts[parts.length - 1].replace('.php', '');
+  
+  runTestProcess(testFilename);
+});
+
+nova.commands.register("phptestsuite.runAll", (editor) => {
+  runTestProcess();
+});
+
+nova.commands.register("phptestsuite.doubleClick", () => {    
+    let selection = treeView.selection;
+    
+    runTestProcess(selection.map((e) => e.name));
+});
+
+function getWorkingDirPath() {
+  let workingDir =
+    nova.workspace.config.get(
+      "tests",
+      "string"
+    ) || nova.workspace.path;
+  if (!workingDir) {
+    return null;
+  }
+  if (!nova.path.isAbsolute(workingDir)) {
+    if (!nova.workspace.path) {
+      return null;
+    }
+    workingDir = nova.path.join(nova.workspace.path, workingDir);
+  }
+  return nova.path.normalize(workingDir);
+}
+
+function getTestRunner() {
+  if (! nova.fs.stat(nova.path.join(nova.workspace.path, 'composer.json'))) {
+    nova.workspace.showErrorMessage('Project must contain composer.json file in order to run tests.');
+    return;
+  }
+  
+  const fileContent = nova.fs.open(nova.path.join(nova.workspace.path, 'composer.json'));
+  
+  return fileContent.read().includes('"pestphp/pest"') ? "vendor/bin/pest" : "vendor/bin/phpunit";
+}
+
+function runTestProcess(test = null) {
+  const args = test ? ["--filter", '"' + test + '"'] : [];
+  
+  const process = new Process(nova.path.join(nova.workspace.path, testRunner), {
+      args,
+      env: {
+          CI: "true"
+      },
+      cwd: getWorkingDirPath(),
+      stdio: ["ignore", "pipe", "pipe"],
+  });
+  
+  let errOutput = "\n\n";
+  
+  try {
+      process.onStdout(function(line) {
+        errOutput += line;
+      });
+      
+      process.start();
+  } catch (e) {
+      console.log(e);
+  }
+  
+  process.onDidExit(() => {
+    nova.workspace.showActionPanel(errOutput);
+  });
+}
+
+class TestItem {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+        this.parent = null;
+    }
+    
+    addChild(element) {
+        element.parent = this;
+        this.children.push(element);
+    }
+}
+
+class TestDataProvider {
+    constructor() {
+        this.rootItems = [];
+        
+        this.findTests(nova.path.join(getWorkingDirPath(), "tests"));
+    }
+    
+    getChildren(element) {
+        // Requests the children of an element
+        if (!element) {
+            return this.rootItems;
+        }
+        else {
+            return element.children;
+        }
+    }
+    
+    getParent(element) {
+        // Requests the parent of an element, for use with the reveal() method
+        return element.parent;
+    }
+    
+    getTreeItem(element) {
+        let item = new TreeItem(element.name);
+        
+        if (element.children.length > 0) {
+            item.collapsibleState = TreeItemCollapsibleState.Collapsed;
+            item.image = "__filetype.php";
+            item.contextValue = "test class";
+        } else {
+            item.command = "phptestsuite.doubleClick";
+            item.contextValue = "test method";
+        }
+        
+        return item;
+    }
+    
+    findTests(path) {
+      let tests = nova.fs.listdir(path);
+      
+      tests.forEach((test) => {
+        let element = new TestItem(test);
+            
+        if (nova.fs.stat(nova.path.join(path, test)).isDirectory()) {
+          return this.findTests(nova.path.join(path, test));
+        }
+        
+        const fileContent = nova.fs.open(nova.path.join(path, test));
+        
+        let hasTests = false;
+        
+        fileContent.readlines().forEach((line) => {
+          const matchPest = line.trim().match(/^\s*(?:it|test)\(([^,)]+)/m);
+
+          const matchUnit = line.trim().match(/^\s*(?:public|private|protected)?\s*function\s*(\w+)\s*\(.*$/);
+            
+          if (matchPest) {
+              element.addChild(new TestItem(matchPest[1].replace(/["']/g, "")));
+              hasTests = true;
+          } else if (matchUnit) {
+              element.addChild(new TestItem(matchUnit[1].replace(/["']/g, "")));
+              hasTests = true;
+          }
+       });
+       
+        if (hasTests) {
+          this.rootItems.push(element);
+        }
+      });
+    }
+}
+
